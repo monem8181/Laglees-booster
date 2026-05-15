aebd5e6881e69a7f3fd5598939098133049a331f
monem8181 <akasacara12@gmail.com>
2026-05-15 22:01:06 +0600

    update: app/src/main/java/com/lagless/booster/MainActivity.kt

--- app/src/main/java/com/lagless/booster/MainActivity.kt
+++ app/src/main/java/com/lagless/booster/MainActivity.kt
@@ -5,43 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.lagless.booster.navigation.LagLessNavGraph
+import com.lagless.booster.ui.theme.BackgroundPrimary
 import com.lagless.booster.ui.theme.LagLessTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install the system splash screen (shows the icon before Compose is ready)
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LagLessTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color    = BackgroundPrimary
+                ) {
+                    val navController = rememberNavController()
+                    LagLessNavGraph(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LagLessTheme {
-        Greeting("Android")
     }
 }
